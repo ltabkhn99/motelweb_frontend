dd7c5af6086eec9f23da154d8a7b45c0ae57b4c6
ltabkhn99 <letuananh7111999@gmail.com>
2024-01-28 10:43:36 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './layouts/header-footer/Navbar';
+import Footer from './layouts/header-footer/Footer';
 
 
 function App() {
   return (
     <div className='App'>
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
